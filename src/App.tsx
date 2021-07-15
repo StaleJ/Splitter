fdbc07bd6f93bd2eba314df9580908ada96b11e8
Ståle <stale.jacobsen@student.uib.no>
2021-07-15 15:52:18 +0200

    inital commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './index.css'
+import Title from "./Components/Title";
+import Card from "./Components/Card";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App min-h-screen" >
+      <Title/>
+        <Card/>
     </div>
   );
 }
---
+++ src/Components/Card.tsx
@@ -0,0 +1,9 @@
+function Card() {
+    return(
+        <div className="Card">
+
+        </div>
+    );
+}
+
+export default Card;
