4d6566ca2a04366798a5619ac64c9e59f3097789
Ståle <stale.jacobsen@student.uib.no>
2021-07-20 14:50:54 +0200

    styling tip amount

--- src/Components/Amount.tsx
+++ src/Components/Amount.tsx
@@ -1,13 +1,13 @@
 function Amount() {
     return (
         <div className="Amount">
-            <h3>Tip Amount</h3>
-            <h4>/ person</h4>
-            <h1>$0.0</h1>
-            <h3>Total</h3>
-            <h4>/ person</h4>
-            <h1>$0.0</h1>
-            <button>Reset</button>
+            <h3 className="Tip-h3">Tip Amount</h3>
+            <h4 className="Tip-h4">/ person</h4>
+            <h1 className="Tip-h1">$0.0</h1>
+            <h3 className="Total-h3">Total</h3>
+            <h4 className="Total-h4">/ person</h4>
+            <h1 className="Total-h1">$0.0</h1>
+            <button className="Amount-Reset">Reset</button>
         </div>
     );
 }
