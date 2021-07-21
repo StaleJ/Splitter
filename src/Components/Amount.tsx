772213c1c866a02fcbb9259902ac4049b5256aea
Ståle <stale.jacobsen@student.uib.no>
2021-07-21 12:29:19 +0200

    Making components and styling

--- src/Components/Amount.tsx
+++ src/Components/Amount.tsx
@@ -1,15 +1,20 @@
-function Amount() {
-    return (
-        <div className="Amount">
-            <h3 className="Tip-h3">Tip Amount</h3>
-            <h4 className="Tip-h4">/ person</h4>
-            <h1 className="Tip-h1">$0.0</h1>
-            <h3 className="Total-h3">Total</h3>
-            <h4 className="Total-h4">/ person</h4>
-            <h1 className="Total-h1">$0.0</h1>
-            <button className="Amount-Reset">Reset</button>
-        </div>
-    );
+import React from "react";
+
+
+class Amount extends React.Component {
+    render() {
+        return (
+            <div className="Amount">
+                <h3 className="Tip-h3">Tip Amount</h3>
+                <h4 className="Tip-h4">/ person</h4>
+                <h1 className="Tip-h1">$0.0</h1>
+                <h3 className="Total-h3">Total</h3>
+                <h4 className="Total-h4">/ person</h4>
+                <h1 className="Total-h1">$0.0</h1>
+                <button className="Amount-Reset">Reset</button>
+            </div>
+        );
+    }
 }
 
 export default Amount;
