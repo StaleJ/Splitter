053776b8f217922c97ab94d2e941d548e586fa95
Ståle <stale.jacobsen@student.uib.no>
2021-07-28 11:20:57 +0200

    useState work, and handle bill

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,41 @@
 import './index.css'
 import IconDollar from "./Static/icon-dollar.svg";
 import IconPerson from "./Static/icon-person.svg";
-import React from "react";
+import React, {useState} from "react";
 
+
+type Tip = {
+    bill: number;
+    selectTip: number;
+    numberOfPeople: number;
+    tipAmount: number;
+    total: number;
+}
+
+
+const defaultTip: Tip = {
+    bill: 0,
+    selectTip: 0,
+    numberOfPeople: 0,
+    tipAmount: 0,
+    total: 0.0
+}
 
 function App() {
+    const [tip, setTip] = useState(defaultTip);
+
+    const onChangeTip = <P extends keyof Tip>(prop: P, value: Tip[P]) => {
+        setTip({...tip, [prop]: value});
+    };
+
+    function handleBill(event: React.ChangeEvent<HTMLInputElement>) {
+        let tipAmount = parseFloat(event.currentTarget.value);
+        if (isNaN(tipAmount)) {
+            onChangeTip('tipAmount', 0)
+        } else onChangeTip('tipAmount', tipAmount);
+        
+    }
+
     return (
         <div className="App min-h-screen">
             <div className="text-center pt-32 uppercase Title text-2xl tracking-widest">
@@ -17,6 +48,7 @@ function App() {
                     <div className="Bill-Input-Field">
                         <img src={IconDollar} alt="Dollar icon" className="Dollar-Icon"/>
                         <input id="number" type="number" placeholder="0" className="Bill-Input"
+                               onChange={handleBill}
                         />
                     </div>
                 </div>
@@ -41,15 +73,16 @@ function App() {
                 <div className="Amount">
                     <h3 className="Tip-h3">Tip Amount</h3>
                     <h4 className="Tip-h4">/ person</h4>
-                    <h1 className="Tip-h1">$0.0</h1>
+                    <h1 className="Tip-h1">${tip.tipAmount}</h1>
                     <h3 className="Total-h3">Total</h3>
                     <h4 className="Total-h4">/ person</h4>
-                    <h1 className="Total-h1">$0.0</h1>
+                    <h1 className="Total-h1">${tip.total}</h1>
                     <button className="Amount-Reset">Reset</button>
                 </div>
             </div>
         </div>
-    );
+    )
+
 }
 
 export default App;
